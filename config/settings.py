2ffe318ce39923154361e572188316d24dc2f035
amahadnezhad <amahadnezhad@gmail.com>
2024-03-15 22:29:14 +0330

    Add Redirect Url To Setting

--- config/settings.py
+++ config/settings.py
@@ -136,4 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authentication Setting
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
