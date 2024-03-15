218f5945ed396d132565e15ef56371d04130641c
amahadnezhad <amahadnezhad@gmail.com>
2024-03-16 00:51:03 +0330

    Add __str__ Magic Method To Post Model

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,6 @@ class Post(models.Model):
     status = models.CharField(choices=STATUS_CHOICES, max_length=3)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
