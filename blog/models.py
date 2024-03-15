5918088bb0711d1f6799e3d28ef9460ed4badc95
amahadnezhad <amahadnezhad@gmail.com>
2024-03-16 00:39:34 +0330

    Create Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
