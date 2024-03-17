8593e32ff24721c08dba1881ac62a29e43f8af42
amahadnezhad <amahadnezhad@gmail.com>
2024-03-17 17:34:38 +0330

    Add date_created Field in Models.py

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+    date_created = models.DateField(auto_now_add=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
