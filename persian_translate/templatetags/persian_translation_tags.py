b676b3c4f7eefae8ccba867f0bb2ef057f0852c9
amahadnezhad <amahadnezhad@gmail.com>
2024-03-26 17:39:31 +0330

    Translate Numbers To Persian Number In posts_list.html Template

--- persian_translate/templatetags/persian_translation_tags.py
+++ persian_translate/templatetags/persian_translation_tags.py
@@ -6,4 +6,5 @@ register = template.Library()
 @register.filter
 def translate_number(value):
     value = str(value)
-    value.maketrans("0123456789", "۰۱۲۳۴۵۶۷۸۹")
+    e_to_p_table = value.maketrans("0123456789", "۰۱۲۳۴۵۶۷۸۹")
+    return value.translate(e_to_p_table)
